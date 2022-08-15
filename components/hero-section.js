f392c4da6a7afba96f010507743d74bf64b9a373
Sarah Zhang <15064710+sazhang@users.noreply.github.com>
2022-08-15 10:15:05 -0700

    update resume

--- components/hero-section.js
+++ components/hero-section.js
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
     <section className="max-w-screen-lg pt-16 pb-20 sm:pt-32 sm:pb-36">
       <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl leading-tight sm:leading-tight md:leading-tight lg:leading-tight">
-        Frontend developer with an eye 👁 for crafting intuitive design, hands 🤙
+        Frontend developer with an eye 👁 for crafting clean designs, hands 🤙
         for building functional interfaces, and intuition 💡 for shaping
         memorable experiences. Currently a product lead working remotely at an{" "}
         <a
